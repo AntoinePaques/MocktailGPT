0d8849d12f60f28841b16b2c041bb905e2a021a6
Antoine Pâques <antoine.paques@gmail.com>
2025-06-11 18:33:04 +0200

    feat: Codex refactor - clean PR

--- packages/@mocktailgpt/ts/src/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export { getOpenAiClientOptions } from './getOpenAiClientOptions';
-export { getSwaggerDoc } from './getSwaggerDoc';
-export { getSwaggerMeta } from './getSwaggerMeta';
-export { getVendorExtensionKeys } from './getVendorExtensionKeys';
