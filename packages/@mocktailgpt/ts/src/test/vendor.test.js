9445ab7814688d66f6b43e246168bac87067eba2
Antoine <antoine.paques@gmail.com>
2025-06-12 07:17:21 +0200

    chore: tidy tests and doc

--- packages/@mocktailgpt/ts/src/test/vendor.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const assert_1 = __importDefault(require("assert"));
-const vendorExtensions_1 = require("../src/vendorExtensions");
-assert_1.default.strictEqual(vendorExtensions_1.VENDOR_EXTENSIONS.MODEL, "x-model");
-console.log("vendor keys ok");
