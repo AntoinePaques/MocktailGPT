c973362ab42b69adf055b2a76f1d5c4fc1949b63
Antoine Pâques <antoine.paques@gmail.com>
2025-06-12 07:02:07 +0200

    Fixes

--- packages/@mocktailgpt/ts/src/test/vendor.test.ts
+++ packages/@mocktailgpt/ts/src/test/vendor.test.ts
@@ -1,5 +1,5 @@
 import assert from "assert";
-import { VENDOR_EXTENSIONS } from "../src/vendorExtensions";
+import {VENDOR_EXTENSIONS} from '../vendorExtensions';
 
 assert.strictEqual(VENDOR_EXTENSIONS.MODEL, "x-model");
 console.log("vendor keys ok");
